41512fcb70bf41e8f9fc336bc11ee9e66a023db6
Ganesh Prasad Chidananda (Industrial GA264577 Engineering Services (IGA264577ES)) <GA264577@wipro.com>
2020-01-21 16:28:20 +0530

    Committing the test file

---
+++ test.py
@@ -0,0 +1,24 @@
+import urllib.request
+with urllib.request.urlopen('https://www.nexus.ford.com/service/rest/repository/browse/fnv2_private_release_repository/com/ford/ecg/ECG_Bundle_Debug/ECG_Bundle_Debug/') as response:
+   html = response.read()
+   print (html)
+
+############################################################################
+
+# import urllib.request
+#
+# print('Beginning file download with urllib2...')
+#
+# url = 'https://publib.boulder.ibm.com/bpcsamp/v6r1/monitoring/clipsAndTacks/download/ClipsAndTacksF1ForModeler.zip'
+# urllib.request.urlretrieve(url, 'C:\\Users\\ga264577\\Desktop\\test\\aa.zip')
+
+#########################################################################################
+
+# import requests
+#
+# url = 'http://gmail.com'
+# user, password = 'ganeshprasad20', 'gan123*#'
+# resp = requests.get(url, auth=(user, password))
+# print (resp.text)
+
+
